004393f7c90a9e647d8ce181c83c330b2190bfb4
Angus Kellett <anguskellett@hotmail.com>
2020-11-19 06:24:43 +0000

    adds function fetchCoordsByIP

--- iss.js
+++ iss.js
@@ -27,4 +27,25 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ip-api.com/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    let coords = {};
+    const lat = JSON.parse(body)['lat'];
+    const lon = JSON.parse(body)['lon'];
+    coords.latitude = lat;
+    coords.longitude = lon;
+    callback(null, coords);
+  });
+};
+
+module.exports = { fetchMyIP,
+  fetchCoordsByIP };
