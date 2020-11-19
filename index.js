ed2b0f057e674d2a79c2abd60b91c9aa77bcaf60
Angus Kellett <anguskellett@hotmail.com>
2020-11-19 06:24:16 +0000

    calls fetchCoordsByIP function

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
+let IPv4 = "";
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
+  IPv4 = ip;
   console.log('It worked! Returned IP:' , ip);
 });
+
+fetchCoordsByIP(IPv4, (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(data);
+});
+
