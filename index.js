6317b5397e0fe3bf7fc5802b0d44b553dd9c8a77
Angus Kellett <anguskellett@hotmail.com>
2020-11-20 07:30:34 +0000

    calls fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 let IPv4 = "";
+let coords = {};
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -10,13 +12,22 @@ fetchMyIP((error, ip) => {
   }
   IPv4 = ip;
   console.log('It worked! Returned IP:' , ip);
+  fetchCoordsByIP(IPv4, (error, data) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    coords = data;
+    console.log('Returned geocoordinates:' , data);
+    fetchISSFlyOverTimes(coords, (error, data) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      console.log('Next five ISS flyovers:' , data);
+    });
+  });
 });
 
-fetchCoordsByIP(IPv4, (error, data) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  console.log(data);
-});
+
 
