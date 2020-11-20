78da1d267af6af6ea01f6db69f0e3bb4a8746a01
Angus Kellett <anguskellett@hotmail.com>
2020-11-20 20:01:33 +0000

    calls a single function, nextISSTimesForMyLocation, and removes previous calls

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-let IPv4 = "";
-let coords = {};
-
-fetchMyIP((error, ip) => {
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
   }
-  IPv4 = ip;
-  console.log('It worked! Returned IP:' , ip);
-  fetchCoordsByIP(IPv4, (error, data) => {
-    if (error) {
-      console.log(error);
-      return;
-    }
-    coords = data;
-    console.log('Returned geocoordinates:' , data);
-    fetchISSFlyOverTimes(coords, (error, data) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      console.log('Next five ISS flyovers:' , data);
-    });
-  });
+  console.log(passTimes);
 });
-
-
-
