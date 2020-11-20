7b581e85740f416d8f1da5eb125b76bf8dc8dddb
Angus Kellett <anguskellett@hotmail.com>
2020-11-20 20:02:25 +0000

    chains three original functions into a single waterfall function nextISSTimesForMyLocation

--- iss.js
+++ iss.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-const fetchMyIP = function(callback) {
+const nextISSTimesForMyLocation = function(callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) {
       callback(error, null);
@@ -12,46 +12,36 @@ const fetchMyIP = function(callback) {
       return;
     }
     const ip = JSON.parse(body)['ip'];
-    callback(null, ip);
+    request(`http://ip-api.com/json/${ip}`, (error, response, body) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      if (response.statusCode !== 200) {
+        const msg = `Status Code ${response.statusCode} when fetching geocoordinates. Response: ${body}`;
+        callback(Error(msg), null);
+        return;
+      }
+      let coords = {};
+      const lat = JSON.parse(body)['lat'];
+      const lon = JSON.parse(body)['lon'];
+      coords.latitude = lat;
+      coords.longitude = lon;
+      request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+        if (error) {
+          callback(error, null);
+          return;
+        }
+        if (response.statusCode !== 200) {
+          const msg = `Status Code ${response.statusCode} when fetching ISS flyover times. Response: ${body}`;
+          callback(Error(msg), null);
+          return;
+        }
+        const passes = JSON.parse(body)['response'];
+        callback(null, passes);
+      });
+    });
   });
 };
 
-const fetchCoordsByIP = function(ip, callback) {
-  request(`http://ip-api.com/json/${ip}`, (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
-    if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching geocoordinates. Response: ${body}`;
-      callback(Error(msg), null);
-      return;
-    }
-    let coords = {};
-    const lat = JSON.parse(body)['lat'];
-    const lon = JSON.parse(body)['lon'];
-    coords.latitude = lat;
-    coords.longitude = lon;
-    callback(null, coords);
-  });
-};
-
-const fetchISSFlyOverTimes = function(coords, callback) {
-  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
-    if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching ISS flyover times. Response: ${body}`;
-      callback(Error(msg), null);
-      return;
-    }
-    const parsed = JSON.parse(body)['response'];
-    console.log(parsed);
-  });
-};
-
-module.exports = { fetchMyIP,
-  fetchCoordsByIP,
-  fetchISSFlyOverTimes };
+module.exports = { nextISSTimesForMyLocation };
