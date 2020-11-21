dd8cc5f0532eb159460548e32d601601de96178a
Angus Kellett <anguskellett@hotmail.com>
2020-11-21 01:03:58 +0000

    creates fetchMyIp and fetchCoordsByIP

--- iss_promised.js
+++ iss_promised.js
@@ -1,1 +1,13 @@
 const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body)['ip'];
+  return request(`http://ip-api.com/json/${ip}`);
+};
+
+module.exports = { fetchMyIP,
+  fetchCoordsByIP };
