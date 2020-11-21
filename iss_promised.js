248f13e6172e098477b9d80c5fff385c9f7a05ae
Angus Kellett <anguskellett@hotmail.com>
2020-11-21 01:39:29 +0000

    adds nextISSTimesForMyLocation function

--- iss_promised.js
+++ iss_promised.js
@@ -9,5 +9,24 @@ const fetchCoordsByIP = function(body) {
   return request(`http://ip-api.com/json/${ip}`);
 };
 
-module.exports = { fetchMyIP,
-  fetchCoordsByIP };
+const fetchISSFlyOverTimes = function(body) {
+  const coords = {};
+  const lat = JSON.parse(body)['lat'];
+  const lon = JSON.parse(body)['lon'];
+  coords.latitude = lat;
+  coords.longitude = lon;
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+
+module.exports = { nextISSTimesForMyLocation };
