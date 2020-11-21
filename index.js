fb368409234989286b5b861db3206fb0782269e7
Angus Kellett <anguskellett@hotmail.com>
2020-11-21 01:04:43 +0000

    removes comments

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ const printPassTimes = function(passTimes) {
 };
 
 nextISSTimesForMyLocation((error, passTimes) => {
-  if (error) {
-    return console.log("It didn't work!", error);
-  }
-  // success, print out the deets!
+if (error) {
+  return console.log("It didn't work!", error);
+}
   printPassTimes(passTimes);
 });
